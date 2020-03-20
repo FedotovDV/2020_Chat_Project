988e0ab58291f0285df0d19b4f2cba4b1903327b
Dmitriy <fed3375546@yandex.ru>
2020-03-21 01:24:50 +0300

    refactorung Main: add Exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,45 +11,30 @@ public class Main {
     public final static int PORT = 8290;
 
     public static void main(String[] args) {
+        String message;
         System.out.println("Hi, Client!");
         try (BufferedReader console = new BufferedReader(
                 new InputStreamReader(System.in));
         ) {
             Socket socket = new Socket(IP, PORT);
 
-            while (true) {
-                String message = console.readLine();
-                System.out.println("I will send " + message);
+            try (BufferedWriter writer = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
+                 BufferedReader serverReader = new BufferedReader(
+                         new InputStreamReader(socket.getInputStream()));) {
+                while (!(message = console.readLine()).equals("Exit")) {
 
-                Thread t = new Thread(() -> {
-                    try {
-                        BufferedWriter writer = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
-                        writer.write(message);
-                        writer.newLine();
-                        writer.flush();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-                t.start();
+                    System.out.println("I will send " + message);
+                    writer.write(message);
+                    writer.newLine();
+                    writer.flush();
+                    System.out.println(serverReader.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
 
-                Thread r = new Thread(() -> {
-                    try {
-                        BufferedReader serverReader = new BufferedReader(
-                                new InputStreamReader(socket.getInputStream()));
 
-                            System.out.println(serverReader.readLine());
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-                r.start();
-
-                t.join();
-                r.join();
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
