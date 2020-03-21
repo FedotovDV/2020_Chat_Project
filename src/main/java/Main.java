66ffb67bb806c31bafb4f99aa609a1301d2e1e1b
Dmitriy <fed3375546@yandex.ru>
2020-03-21 16:03:29 +0300

    it works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,31 +11,52 @@ public class Main {
     public final static int PORT = 8290;
 
     public static void main(String[] args) {
-        String message;
+
         System.out.println("Hi, Client!");
         try (BufferedReader console = new BufferedReader(
                 new InputStreamReader(System.in));
         ) {
             Socket socket = new Socket(IP, PORT);
+            Thread t = new Thread(() -> {
+                try (BufferedWriter writer = new BufferedWriter(new PrintWriter(socket.getOutputStream()))) {
 
-            try (BufferedWriter writer = new BufferedWriter(new PrintWriter(socket.getOutputStream()));
-                 BufferedReader serverReader = new BufferedReader(
-                         new InputStreamReader(socket.getInputStream()));) {
-                while (!(message = console.readLine()).equals("Exit")) {
+                    while (true) {
+                        String message = console.readLine();
+                        System.out.println("I will send " + message);
+                        writer.write(message);
+                        writer.newLine();
+                        writer.flush();
+                    }
 
-                    System.out.println("I will send " + message);
-                    writer.write(message);
-                    writer.newLine();
-                    writer.flush();
-                    System.out.println(serverReader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            });
 
 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            Thread r = new Thread(() -> {
+                try (
+                        BufferedReader serverReader = new BufferedReader(
+                                new InputStreamReader(socket.getInputStream()))) {
+
+                    while (true) {
+                        System.out.println(serverReader.readLine());
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            t.start();
+            r.start();
+            t.sleep(1000);
+            t.sleep(1000);
+            t.join();
+            r.join();
+            socket.close();
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
         }
+
     }
 }
